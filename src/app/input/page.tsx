0f3806ad27b17c917d032a075ac85b68f3c4d1ed
이현욱 <dldnr789@gmail.com>
2024-04-24 21:31:48 +0900

    [Init] : 조사 반영되도록 수정

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -1,18 +1,52 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import { sampleAtom } from '@/recoil/atom'
+import { usePathname, useRouter } from 'next/navigation'
 
 const Home = () => {
   const sample = useRecoilValue(sampleAtom)
+  const router = useRouter()
+  const [nickname, setNickname] = useState('')
+
+  const getRelationship = (name) => {
+    if (typeof name !== 'string') return null
+
+    var lastLetter = name[name.length - 1]
+    var uni = lastLetter.charCodeAt(0)
+
+    if (uni < 44032 || uni > 55203) return null
+
+    return (uni - 44032) % 28 != 0
+  }
+
+  const handleNicknameChange = (event) => {
+    setNickname(event.target.value)
+  }
+
+  const handleNextClick = () => {
+    const isEndingWithBatchim = getRelationship(nickname)
+    const newNM = isEndingWithBatchim ? `${nickname}이랑` : `${nickname}랑`
+    alert(newNM)
+
+    router.push('/mbti')
+  }
 
   return (
     <div>
       <div>유저 정보를 입력받는 페이지 입니다</div>
-      닉네임 : <input type="text" placeholder="내용을 입력해 주세요"></input>
+      <br></br>
+      닉네임 :{' '}
+      <input
+        type="text"
+        placeholder="내용을 입력해 주세요"
+        value={nickname}
+        onChange={handleNicknameChange}
+      ></input>
+      <br></br>
       생년월일 : <input type="text" placeholder="내용을 입력해 주세요"></input>
       <br></br>
-      <a href="/mbti">다음</a>
+      <button onClick={handleNextClick}>다음</button>
     </div>
   )
 }
