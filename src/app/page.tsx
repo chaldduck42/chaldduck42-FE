d54c83d5c1831df73ca49a1611d8ecba7577e015
asylee02 <87488288+asylee02@users.noreply.github.com>
2024-04-24 16:21:59 +0900

    Revert "Feat/button"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import { useRecoilValue } from 'recoil'
 import { sampleAtom } from '@/recoil/atom'
-import Button from '@/components/button/DarkButton'
 
 const Home = () => {
   const sample = useRecoilValue(sampleAtom)
@@ -11,7 +10,6 @@ const Home = () => {
     <div>
       <div>Recoil 값: {sample}</div>
       <a href="/input">내 사주 알아보기</a>
-      <Button width="14" height="80" label="hello" />
     </div>
   )
 }
