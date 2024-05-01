9c6d9defc74e7d62a189f5bc5ed2da51665f23ee
youngduck <dudejr5839@gmail.com>
2024-05-01 21:55:50 +0900

    [init] : erd type 초기세팅

---
+++ src/types/saju.ts
@@ -0,0 +1,13 @@
+export interface ISajuPictureDescription {
+  id: number
+  saju_1: number
+  saju_2: number
+  image: string
+  description: string
+}
+export interface ISajuResult {
+  id: number
+  five_hang: string
+  image: string
+  description: string
+}
