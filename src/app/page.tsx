15dc3fb497efe1a14dc52397db3892e2fad3d3f2
sangyeon Lee <asylee02@gmail.com>
2024-07-17 03:51:57 +0900

    feat: msw 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 'use client'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    axios.get('http://localhost:9090/api/test').then((res) => console.log(res))
+  }, [])
   return (
     <div>
       <a href="/input">내 사주 알아보기</a>
