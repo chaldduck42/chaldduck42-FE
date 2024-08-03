6239ddf0e49ab7b67cc48b51e7ad8c1c88b69be2
sangyeon Lee <asylee02@gmail.com>
2024-08-03 16:58:06 +0900

    fix: global.css 오타

--- src/components/mbti/SelectContent.tsx
+++ src/components/mbti/SelectContent.tsx
@@ -1,4 +1,7 @@
+'use client'
 import { QuestionList } from '@/types/mbti'
+import mbtiCaculate from '@/utils/mbtiCalculate'
+import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
@@ -8,7 +11,7 @@ interface Props {
 }
 const SelectContent = ({ id, info }: Props) => {
   const router = useRouter()
-  const nickname = localStorage.getItem('userInfo ')
+  const nickname = localStorage.getItem('userInfo')
   const handlePrev = () => {
     router.push(`/mbti/${id - 1}`)
   }
@@ -23,7 +26,6 @@ const SelectContent = ({ id, info }: Props) => {
     } else {
       router.push(`/mbti/${id + 1}`)
     }
-
   }
   return (
     <div className="w-full flex flex-col justify-between items-center mt-[56px]">
@@ -33,26 +35,27 @@ const SelectContent = ({ id, info }: Props) => {
       <div className="w-full pt-[94px]">
         <button
           type="button"
-          value={info.question}
+          value={info.answers[0].score}
           className="w-full rounded-xl min-h-[56px] text-[16px] font-bold text-Dark-Brown bg-Beige3 mb-[32px] hover:bg-Dark-Brown hover:text-white flex justify-start items-center px-[16px] animate-fade-left"
-          onClick={handleNext}
+          onClick={() => handleNext(String(info.answers[0].score))}
         >
           {info.answers[0].option}
         </button>
         <button
           type="button"
+          value={info.answers[1].score}
           className="w-full rounded-xl min-h-[56px] text-[16px] font-bold text-Dark-Brown bg-Beige3 hover:bg-Dark-Brown hover:text-white flex justify-start items-center px-[16px] animate-fade-left"
-          onClick={handleNext}
+          onClick={() => handleNext(String(info.answers[1].score))}
         >
           {info.answers[1].option}
         </button>
       </div>
-      {id === 0 ? (
+      {id === 1 ? (
         <div className="w-full rounded-xl h-16 " />
       ) : (
         <button
           type="button"
-          className="w-full rounded-xl min-h-[56px] text-[16px] font-bold text-Dark-Brown border-[1px] bord er-Dark-Brown hover:bg-Dark-Brown hover:text-white absolute bottom-[64px] animate-fade"
+          className="w-full rounded-xl min-h-[56px] text-[16px] font-bold text-Dark-Brown border-[1px] border-Dark-Brown hover:bg-Dark-Brown hover:text-white absolute bottom-[64px] animate-fade"
           onClick={handlePrev}
         >
           이전
